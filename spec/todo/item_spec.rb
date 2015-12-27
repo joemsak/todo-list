158c4d050b51475bd20d54900c1dc31773d13ae4
Joe Sak <joe@joesak.com>
2015-12-27 10:33:21 -0500

    Make Todo Items completable

--- spec/todo/item_spec.rb
+++ spec/todo/item_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 Todo::Item = Struct.new(:text) do
+  def mark_complete
+    @completed = true
+  end
+
   def completed?
     @completed ||= false
   end
@@ -10,5 +14,11 @@ RSpec.describe Todo::Item do
   it 'is not completed' do
     item = described_class.new('Write a failing test')
     expect(item).not_to be_completed
+  end
+
+  it 'can be marked as complete' do
+    item = described_class.new('Make items completable')
+    item.mark_complete
+    expect(item).to be_completed
   end
 end
