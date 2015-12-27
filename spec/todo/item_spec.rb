b66abd1a512d83538861ee1eb68309a50420962c
Joe Sak <joe@joesak.com>
2015-12-27 10:33:31 -0500

    Make todo items assignable

--- spec/todo/item_spec.rb
+++ spec/todo/item_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 Todo::Item = Struct.new(:text) do
+  attr_reader :assignee
+
+  def assign_to(assignee)
+    @assignee = assignee
+  end
+
   def mark_complete
     @completed = true
   end
@@ -20,5 +26,11 @@ RSpec.describe Todo::Item do
     item = described_class.new('Make items completable')
     item.mark_complete
     expect(item).to be_completed
+  end
+
+  it 'can be assigned to someone' do
+    item = described_class.new('Make items assignable')
+    item.assign_to('Rick')
+    expect(item.assignee).to eq('Rick')
   end
 end
